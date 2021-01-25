80733c97d48faad03f74a1bfd94d55b580afd952
Dustin Brinkman <mimirs.spring@gmail.com>
2021-01-25 18:22:24 -0500

    mvp

---
+++ src/tests/app.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+// jest.mock('../api/fecthShow');
+
+// import {fetchShow as mockFetchShow} from '../api/fetchShow';
+
+// test('Renders App without errors', ()=> {
+//     render(<App/>)
+// });
+
+
+test('Fetches data correctly when button is clicked', async()=> {
+ 
+ render(<App/>)
+ await waitFor(async()=> {
+    const dropdown = await screen.findByText(/select a season/i);
+    console.log(dropdown);
+    userEvent.click(dropdown);
+    expect(screen.getByText('Season 1')).toBeInTheDocument();
+    const season2 = await screen.findByText(/Season 2/i);
+    userEvent.click(season2)
+    expect(screen.getByText('Season 2')).toBeInTheDocument();
+
+    // fireEvent.change(dropdown, {
+    //     target: {value: 'Season 1'},
+    // });
+    // expect(screen.getByText('Season 1')).toBeInTheDocument();
+
+    // fireEvent.change(screen.getAllByTestId('dropdown'), {
+    //     target: {value:'Season 2'}
+    // });
+    // expect(screen.getByText('Season 2')).toBeInTheDocument();
+    // console.log('Testing')
+});
+
+
+});
+
+
+
